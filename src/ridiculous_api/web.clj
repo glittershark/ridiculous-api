271b52d41ec9768973315c98312622a2ee50351e
Griffin Smith <wildgriffin45@gmail.com>
2014-12-13 17:17:18 -0500

    Migrate before starting server

--- src/ridiculous_api/web.clj
+++ src/ridiculous_api/web.clj
@@ -5,6 +5,8 @@
             [compojure.route :as route]
             [compojure.core :refer [GET POST defroutes]]
             [ring.util.response :refer [resource-response response]]
+            [lobos.config :as config]
+            [lobos.core :refer [migrate]]
             [ring.middleware.json :as middleware]))
 
 (defroutes app-routes
@@ -18,5 +20,7 @@
       (middleware/wrap-json-response)))
 
 (defn start [port]
+  (config/init)
+  (migrate)
   (ring/run-jetty app {:port port
                        :join? false}))
